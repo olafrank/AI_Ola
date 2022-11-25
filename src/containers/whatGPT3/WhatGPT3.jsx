50e176b1da53b506b5c469582e2ea2387fb362ed
olafrank <olafrnk@gmail.com>
2022-11-25 13:47:41 +0100

    gpt3 structure created while waiting for feature component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatgpt3.css';
 
-const whatgpt3 = () => {
+const Whatgpt3 = () => {
   return (
-    <div>
-      
+    <div className="gpt3__whatgpt3 section__margin" id='wgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+      <Feature />
+      <Feature />
+      <Feature />
+      </div>
     </div>
   )
 }
 
-export default whatgpt3
+export default Whatgpt3

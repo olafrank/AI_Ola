dd89ee4de481b852582ed602d3b996a079ad88a6
olafrank <olafrnk@gmail.com>
2022-11-30 20:42:03 +0100

    features component ongoing

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
 
-const features = () => {
+const featuresData = [
+  {
+    title: 'improving end distrusts instantly',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo reprehenderit similique repudia'
+  },
+  {
+    title: 'Becoming the tended active',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo reprehenderit similique repudia'
+  },
+  {
+    title: 'message or am nothing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo reprehenderit similique repudia'
+  },
+  {
+    title: 'will be fun having you here',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo reprehenderit similique repudia'
+  },
+
+]
+
+const Features = () => {
   return (
-    <div>
-      
+    <div className='gpt3__features section__padding' id='features'>
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is now and you just need to realize it.set into the future today and make it happen</h1>
+        <p>Request early access to get started</p>
+      </div>
+      <div className="gpt3__features-container">
+      {featuresData.map((item,index) =>(
+          <Feature title ={item.title}  text = {item.text} key ={item.title + index} />
+        )) }
+      </div>
     </div>
   )
 }
 
-export default features
+export default Features
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,7 @@ const Whatgpt3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id='wgpt3'>
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT3" text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, enim voluptatem dolorem numquam placeat, expli Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, enim voluptatem dolorem numquam placeat, explicabo saepe ullam reiciendis excepturi molestias reprehenderit voluptat" />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -15,12 +15,14 @@ const Whatgpt3 = () => {
         <p>Explore the Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-      <Feature />
-      <Feature />
-      <Feature />
+      <Feature title="Chatbots" text="This is an automate chatbox,you can ask your questions and get instant responds,Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi et neque tenetur maxime nesciunt ratione unde minima autem doloremque quas." />
+      <Feature title='Knowledgebase' text='You can learn more about this AI built,Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi et neque tenetur maxime nesciunt ratione unde minima autem doloremque quas.' />
+      <Feature title='Education' text='This is an educative built that you can practice with and do,Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi et neque tenetur maxime nesciunt ratione unde minima autem doloremque quas.  ' />
       </div>
     </div>
   )
 }
+
+
 
 export default Whatgpt3
